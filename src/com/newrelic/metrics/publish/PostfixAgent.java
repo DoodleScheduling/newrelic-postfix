0da5015cf57e575f17e1c4cfe2855b55b0531262
Daniel Gil <dgb@doodle.com>
2016-10-17 10:05:50 +0200

    fix: race condition between reading the process output and process.waitFor()

--- src/com/newrelic/metrics/publish/PostfixAgent.java
+++ src/com/newrelic/metrics/publish/PostfixAgent.java
@@ -19,6 +19,8 @@ public class PostfixAgent extends Agent {
     private static final String GUID = "com.doodle.postfix";
     private static final String VERSION = "1.0.0";
     private final String name;
+    private final static Pattern patternBytes = Pattern.compile(POSTQUEUE_REGEX_BYTES);
+    private final static Pattern patternRequests = Pattern.compile(POSTQUEUE_REGEX_REQUESTS);
 
     public PostfixAgent(String name) throws ConfigurationException {
         super(GUID, VERSION);
@@ -33,9 +35,8 @@ public class PostfixAgent extends Agent {
 
     @Override
     public void pollCycle() {
-        Pattern patternBytes = Pattern.compile(POSTQUEUE_REGEX_BYTES);
-        Pattern patternRequests = Pattern.compile(POSTQUEUE_REGEX_REQUESTS);
         String queue = getPostfixQueue();
+        logger.debug("Parsing output: " + queue);
         Matcher matcherBytes = patternBytes.matcher(queue);
         Matcher matcherRequests = patternRequests.matcher(queue);
         Long kbytes = matcherBytes.find() ? Long.valueOf(matcherBytes.group(1)) : 0;
@@ -53,12 +54,31 @@ public class PostfixAgent extends Agent {
         try {
             ProcessBuilder pb = new ProcessBuilder("postqueue", "-p");
             Process process = pb.start();
-            if (process.waitFor() == 0) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    lastLine = line;
+            String line;
+
+            // Read standard output, and save only the last line
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line = br.readLine()) != null) {
+                lastLine = line;
+            }
+
+            // Read standard error
+            br = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            StringBuilder output = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                output.append(line);
+                output.append(System.getProperty("line.separator"));
+            }
+
+            // Wait for the command to finish
+            int returnCode = process.waitFor();
+            if (returnCode != 0) {
+                logger.error("Command returned code: " + returnCode);
+                if (output.length() > 0) {
+                    logger.error("Standard error:");
+                    logger.error(output);
                 }
+                throw new RuntimeException();
             }
         } catch (Exception e) {
             logger.error("Failed to execute 'postqueue -p'");
